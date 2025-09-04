14d74fea13282d254c511b0d28ecf4ae58c4e622
Somyaditya (Om) <soch2028@Somyadityas-MacBook-Air-2178.local>
2025-09-04 08:49:23 +0530

    modified and new file

--- js/search.js
+++ js/search.js
@@ -1,3 +1,22 @@
-let availableKeywords = ["rackets", "shoes", "accessories"];
+let availableKeywords = [rackets, shoes, accessories];
 const resultsBox = document.querySelector(".result-box");
 const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function () {
+  let result = [];
+  let input = inputBox.value;
+  if (input.length) {
+    result = availableKeywords.filter((keyword) => {
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+    console.log(result);
+  }
+  display(result);
+};
+function display(result) {
+  const content = result.map((list) => {
+    return "<li>" + list + "</li>";
+  });
+
+  resultsBox.innerHTML = "<ul>" + content + "</ul>";
+}
